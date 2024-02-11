9ed1e20f73e48481482d4c17696e210782fd59a4
hrishi2710 <hrishirules2710@gmail.com>
2024-02-11 18:47:57 +0530

    Added tailwind and basic navbar experiment for responsive design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="box-border border-##00a79d border-2 w-1/2 h-screen md:hidden shadow-2xl shadow-cyan-700">
+        Side Menu item
+      </div>
+      <div className="hidden md:block">
+        Navbar
+      </div>
+    </>
   );
 }
 
