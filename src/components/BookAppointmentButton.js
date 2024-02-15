31ef6b3ab44468893eb6dc6ea2ca75510a75a6ba
Hrishikesh kumar <hrishirules2710@gmail.com>
2024-02-15 22:57:55 +0530

    Added bounce animation for the book button

--- src/components/BookAppointmentButton.js
+++ src/components/BookAppointmentButton.js
@@ -1,6 +1,6 @@
 let BookAppointmentButton = (props) => {
     return (
-        <button className="fixed bottom-0 left-1/4 z-5 box-border border-2 bg-teal-600 shadow-2xl text-lg p-3 rounded-lg md:hidden">
+        <button className="fixed bottom-0 left-1/4 z-5 box-border border-2 bg-teal-600 text-white shadow-2xl text-lg p-3 rounded-lg animate-bounce-css md:hidden">
             Book an appointment
         </button>
     )
