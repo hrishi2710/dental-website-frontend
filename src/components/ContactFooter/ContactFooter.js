58e6f0f2f7fa896473d6b148fdf8a7443e838a4a
Hrishikesh Kumar <hrishi@Hrishikeshs-MacBook-Pro.local>
2025-07-21 20:47:00 +0530

    Got rid of footer menu

--- src/components/ContactFooter/ContactFooter.js
+++ src/components/ContactFooter/ContactFooter.js
@@ -17,12 +17,12 @@ const ContactFooter = () => (
   <footer className="w-full bg-[#f8f1ee] py-8 px-2">
     <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center md:items-start">
       {/* Left: Logo and Socials */}
-      <div className="flex flex-col items-center md:items-start w-full md:w-1/3 mb-6 md:mb-0">
+      <div className="flex flex-col items-center md:items-start w-full md:w-1/3 mb-16 md:mb-0">
         <img src={Logo} alt="Logo" className="h-20 w-auto mb-4" />
         <SocialIcons socialLinks={socialLinks} />
       </div>
       {/* Right: Menu */}
-      <FooterMenu leftMenu={leftMenu} rightMenu={rightMenu} />
+      {/* <FooterMenu leftMenu={leftMenu} rightMenu={rightMenu} /> */}
     </div>
   </footer>
 );
