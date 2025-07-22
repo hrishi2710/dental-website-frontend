113e95a34f25e9d2f628fc056fa134382db84c46
Hrishikesh Kumar <hrishi@Hrishikeshs-MacBook-Pro.local>
2025-07-22 21:38:55 +0530

    Added home page route on the logo

--- src/components/LogoSection.js
+++ src/components/LogoSection.js
@@ -1,11 +1,14 @@
 import Logo from '../assets/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const LogoSection = ({ openSideDrawerHandler }) => (
   <div className="w-full md:w-auto md:flex md:flex-col bg-[#f8f1ee] p-0">
     <div className="flex flex-row items-center justify-between w-full py-2 md:block px-4">
-      <img src={Logo} alt="Logo OM DentCare" className="h-14 md:h-20 w-auto object-contain ml-0" />
+      <Link to="/">
+        <img src={Logo} alt="Logo OM DentCare" className="h-14 md:h-20 w-auto object-contain ml-0 cursor-pointer" />
+      </Link>
       <div className="md:hidden flex items-center mr-0" onClick={openSideDrawerHandler}>
         <FontAwesomeIcon icon={faBars} size="2x" className="text-[#b97a56]" />
       </div>
